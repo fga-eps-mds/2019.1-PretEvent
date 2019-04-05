c922e3eff0c42d5652a17b7f179b5ecad9ae43dc
Ian Rocha <ianrocha.df@gmail.com>
2019-04-04 23:09:59 -0300

    Criação da Models e Views do Player.
    Co-authored-by: AGoretti <andremotta96@gmail.com>

--- service/mc1/players/models.py
+++ service/mc1/players/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.validators import MinLengthValidator
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Player(models.Model):
+    #id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    university_id = models.IntegerField()
+    points = models.IntegerField()
+    photo_url = models.CharField()
--- service/mc1/players/views.py
+++ service/mc1/players/views.py
@@ -2,5 +2,29 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from players.models import Player
+
+class PlayerList(ListView):
+    model = Player
+
+class PlayerView(DetailView):
+    model = Player
+
+class PlayerCreate(CreateView):
+    model = Player
+    fields = ['id','name', 'university_id', 'points', 'photo_url']
+    success_url = reverse_lazy('player_list')
+
+class PlayerUpdate(UpdateView):
+    model = Player
+    fields = ['id','name', 'university_id', 'points', 'photo_url']
+    success_url = reverse_lazy('player_list')
+
+class PlayerDelete(DeleteView):
+    model = Player
+    success_url = reverse_lazy('player_list')
