6f5a8eed615e7eb16eec9be35a06b376d3bde100
Ian Rocha <ianrocha.df@gmail.com>
2019-04-05 11:34:35 -0300

    Modificação no CRUD do player
    Co-authored-by: Agoretti <andremotta96@gmail.com>
    Co-authored-by: b2198 <onerb98@hotmail.com>

--- service/mc1/players/views.py
+++ service/mc1/players/views.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -13,15 +8,12 @@ from players.models import Player
 
 class PlayerList(ListView):
     model = Player
-
 class PlayerView(DetailView):
     model = Player
-
 class PlayerCreate(CreateView):
     model = Player
     fields = ['id','name', 'university_id', 'points', 'photo_url']
     success_url = reverse_lazy('player_list')
-
 class PlayerUpdate(UpdateView):
     model = Player
     fields = ['id','name', 'university_id', 'points', 'photo_url']
@@ -29,8 +21,9 @@ class PlayerUpdate(UpdateView):
 
 class PlayerDelete(DeleteView):
     model = Player
+    success_url = reverse_lazy('player_list') 
     success_url = reverse_lazy('player_list')
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all().order_by('-date_joined')
-    serializer_class = PlayerSerializer
+    serializer_class = PlayerSerializer 
