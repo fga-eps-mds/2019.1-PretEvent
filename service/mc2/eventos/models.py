7bd6a2cca621d865bb7687e121f572c54a26effa
Murilo Loiola <messhman.dmc@gmail.com>
2019-04-13 14:08:33 -0300

    Criação das models e views.
    Co-authored-by: IanPSRocha <ianrocha.df@gmail.com>

--- service/mc2/eventos/models.py
+++ service/mc2/eventos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Evento(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    points = models.IntegerField()
+    description = models.TextField()
+
 # Create your models here.
--- service/mc2/eventos/views.py
+++ service/mc2/eventos/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from eventos.models import Evento
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
+from eventos.serializers import EventoCreateSerializer
 
+class EventList(APIView):
+
+    queryset = Evento.objects.all()
+    serializer_class = EventoCreateSerializer
+
+    def get_extra_actions():
+        return []
+
+    def get(self, request, format=None):
+        eventos = Evento.objects.all()
+        serializer = EventoCreateSerializer(eventos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = EventoCreateSerializer(data = request.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 # Create your views here.
