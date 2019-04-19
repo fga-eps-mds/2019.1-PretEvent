95fed7620e635b7e276058829e13707e25687b9d
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2019-04-18 21:36:44 -0300

    F02 Issue #57 criação do componente inicial

---
+++ front-end/pret-event/src/app/player-registration/player-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-registration',
+  templateUrl: './player-registration.component.html',
+  styleUrls: ['./player-registration.component.css']
+})
+export class PlayerRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
