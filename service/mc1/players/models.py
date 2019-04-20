eee314b2484463d4e69217653ab9961e2d74f18a
Murilo Loiola <messhman.dmc@gmail.com>
2019-04-20 13:55:11 -0300

    F17 #87 Usuário agora possui campo de senha e endpoint de login disponível.
    Co-authored-by: IanPSRocha <ianrocha.df@gmail.com>

--- service/mc1/players/models.py
+++ service/mc1/players/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from django.conf import settings
 
-class Player(models.Model):
-    name = models.CharField(max_length=200)
+class Player(User):
     university_id = models.IntegerField()
     points = models.IntegerField()
     photo_url = models.CharField(max_length=200)
