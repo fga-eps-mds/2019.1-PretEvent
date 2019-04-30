ce6ee9c58449072af2b13d25e2c65d03f336da97
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2019-04-30 12:57:05 -0300

    F18 - Login de usuário #118
    Co-authored-by: Breno Alencar <onerb98@hotmail.com>
    Co-authored-by: Djorkaeff Alexandre <djorkaeff7@icloud.com>

--- front-end/pret-event/src/app/common/sign-modal/sign-modal.component.ts
+++ front-end/pret-event/src/app/common/sign-modal/sign-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Player } from '../../models/player';
+import { User } from '../../models/user';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-sign-modal',
@@ -12,10 +16,42 @@ export class SignModalComponent implements OnInit {
 
   typePassword = false;
   signup = false;
+  playerForm: FormGroup;
+  selectedFiles: FileList;
+  file: any;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private service: PlayerService) { }
 
-  ngOnInit() { }
+  registerPlayer() {
+    const player: Player = new Player(
+      this.playerForm.get('name').value,
+      this.playerForm.get('universityId').value,
+      0
+    );
+
+    this.service.registerPlayer(player, this.file).then(x => console.log(x));
+  }
+
+  loginPlayer() {
+    const user: User = new User(
+      this.playerForm.get('name').value,
+      this.playerForm.get('password').value,
+    );
+    this.service.loginPlayer(user).then(x => console.log(x));
+  }
+
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+    this.file = this.selectedFiles[0];
+  }
+
+  ngOnInit() {
+    this.playerForm = this.formBuilder.group({
+      name: '',
+      universityId: '',
+      password: '',
+    });
+  }
 
   showPassword() {
     this.typePassword = !this.typePassword;
--- front-end/pret-event/src/app/services/player.service.ts
+++ front-end/pret-event/src/app/services/player.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Player } from '../models/player';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { finalize } from 'rxjs/operators';
-import { Upload } from '../models/upload';
 
 @Injectable({
   providedIn: 'root'
@@ -17,63 +16,36 @@ export class PlayerService {
 
   constructor(private http: HttpClient, private afStorage: AngularFireStorage) { }
 
-  // GET - Traz da API uma lista com todos os players 
-  getAllPlayers() {
-    this.http.get<Player[]>('/api/players/')
-      .subscribe(
-        data => {
-          // console.log(data);
-          return data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-  }
+  loginPlayer = player =>
+    new Promise(resolve =>
+      this.http.post('/rest-auth/login/', player)
+        .subscribe(
+          data => resolve(data),
+          error => resolve(error),
+        )
+    )
 
-  // GET - Busca na API um player pela sua ID
-  getPlayer(id: number) {
-    this.http.get<Player>('/api/players/' + id)
-      .subscribe(
-        data => {
-          console.log(data);
-          return data;
-        },
-        error => {
-          console.log(error);
-          return error;
-        }
-      );
-  }
+  postPlayer = (player: Player) =>
+    new Promise(resolve =>
+      this.http.post(this.url, player)
+        .subscribe(
+          data => resolve(data),
+          error => resolve(error),
+        )
+    )
 
-  // POST - Salva na API um registro de player
-  postPlayer(player: Player) {
-    this.http.post('api/players/', player)
-      .subscribe(
-        data => {
-          console.log('Post Player: ' + data);
-        },
-        error => {
-          console.log(error);
-        }
-      );
-  }
-
-  // Registrar o player - Sobe sua foto para o firebase e depois chama o metodo postPlayer
-  registerPlayer(player: Player, file: any) {
-
-    this.ref = this.afStorage.ref(player.university_id.toString());
-    this.task = this.ref.put(file);
-
-    this.task.snapshotChanges().pipe(
-      finalize(() => {
-        this.ref.getDownloadURL().subscribe(ref => {
-          this.downloadURL = ref;
-          player.photo_url = this.downloadURL;
-          console.log('UploadPlayerPhoto: ' + this.downloadURL);
-          this.postPlayer(player);
-        });
-      })
-    ).subscribe();
-  }
+  registerPlayer = (player: Player, file: any) =>
+    new Promise(resolve => {
+      this.ref = this.afStorage.ref(player.university_id.toString());
+      this.task = this.ref.put(file);
+      this.task.snapshotChanges().pipe(
+        finalize(() => {
+          this.ref.getDownloadURL().subscribe(ref => {
+            this.downloadURL = ref;
+            player.photo_url = this.downloadURL;
+            resolve(this.postPlayer(player));
+          });
+        })
+      ).subscribe();
+    })
 }
