6429ec2b3197eeb8e242cb3589eb053d2998be8d
Rdadamos <rodrigodadamos@gmail.com>
2019-05-03 03:25:18 -0300

    T35 Issue #108 nginx

--- docker-push.sh
+++ docker-push.sh
@@ -2,6 +2,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push pretevent/app
-# docker push pretevent/web1
-# docker push pretevent/web2
-# docker push pretevent/web3
+docker push pretevent/web1
+docker push pretevent/web2
+docker push pretevent/web3
