4d6bf81065b2be58ae078404eef623528363def9
Murilo Loiola <messhman.dmc@gmail.com>
2019-06-14 18:41:08 -0300

    T63 #172 Atualizando página home para apresentar o local do evento.

--- front-end/pret-event/src/app/models/event.ts
+++ front-end/pret-event/src/app/models/event.ts
@@ -2,6 +2,7 @@ export class Event {
   id: number;
   title: string;
   date: string;
+  place: string;
   points: number;
   description: string;
   // tslint:disable-next-line: variable-name
@@ -10,9 +11,10 @@ export class Event {
   reward_id: number;
   creator_id: number;
 
-  constructor(title: string, date: string, time: string, points: number, description: string, rewardId: number, creatorId: number, photoUrl?: string) {
+  constructor(title: string, date: string, place: string, time: string, points: number, description: string, rewardId: number, creatorId: number, photoUrl?: string) {
     this.title = title;
     this.date = date + "T" + time;
+    this.place = place;
     this.points = points;
     this.description = description;
     this.url_image = photoUrl || '';
