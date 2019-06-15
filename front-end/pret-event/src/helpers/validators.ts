03a125d5d9c9d70c5825b6bfab5df973bb10a4a2
Ian Rocha <ianrocha.df@gmail.com>
2019-06-15 16:44:28 -0300

    T66 #175 Atualizando validação ao campo de local do evento
    Co-authored-by: Djorkaeff Alexandre <djorkaeff.unb@gmail.com>

--- front-end/pret-event/src/helpers/validators.ts
+++ front-end/pret-event/src/helpers/validators.ts
@@ -1,5 +1,6 @@
 export const validateEvent = (event, file) => {
   if (event.title === '') return 'Nome é obrigatório!';
+  if (event.place === '') return 'Local é obrigatório!';
   if (event.date === '' || event.date === 'T' || event.date.split('T').length < 2) return 'Data e hora são obrigatórios!';
   if (event.description === '') return 'Descrição é obrigatória!';
   if (!file) return 'Imagem é obrigatória!';
