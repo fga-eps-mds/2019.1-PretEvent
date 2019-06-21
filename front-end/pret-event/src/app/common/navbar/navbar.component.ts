90d92982beec8f09944eac9fa43f8f8b90cfd417
Agoretti <andremotta96@gmail.com>
2019-06-21 18:11:04 -0300

    recuperando o nome na navbar

--- front-end/pret-event/src/app/common/navbar/navbar.component.ts
+++ front-end/pret-event/src/app/common/navbar/navbar.component.ts
@@ -4,17 +4,22 @@ import { AlertComponent } from 'ngx-bootstrap/alert/alert.component';
 
 import { AlertService } from '../../services/alert.service';
 import { getToken, removeToken } from '../../helpers/token';
-import { removeId } from 'src/app/helpers/id';
+import { removeId, getId } from 'src/app/helpers/id';
+import { Player } from 'src/app/models/player';
+import { PlayerService } from 'src/app/services/player.service';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar ',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
+  
+  
 })
 export class NavbarComponent implements OnInit {
 
   logged = getToken() !== null;
-  account =  this.logged ? 'Sair' : 'Entrar/Cadastrar';
   navbarOpen = false;
 
   modalRef: BsModalRef;
@@ -25,11 +30,22 @@ export class NavbarComponent implements OnInit {
     animated: true,
   };
   alerts: any[] = [];
+  player: Player;
+  name: string;
+  currentId = +getId();
+  account =  this.logged ? 'Sair' : 'Entrar/Cadastrar';
+  router: Router;
 
-  constructor(private modalService: BsModalService, private data: AlertService) {}
+
+  constructor(private modalService: BsModalService, private data: AlertService, private playerservice: PlayerService) {}
 
   ngOnInit() {
     this.data.currentAlert.subscribe(alert => this.alerts.push(alert));
+    this.playerservice.getPlayerid(this.currentId)
+    .then((player: Player) => {
+      this.player = player;
+      this.name = this.player.username;
+    })
   }
 
   onClosed(dismissedAlert: AlertComponent): void {
